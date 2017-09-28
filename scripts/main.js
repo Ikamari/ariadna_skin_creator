feb7fee756987889df8ccf169f3c23458bfd6673
Ikamari <SilverFire098@gmail.com>
2017-09-29 00:09:05 +0300

    Little changes in index.html and main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,33 @@
-/**
- * Created by Ikamari on 28.09.2017.
- */
+//TODO: UI, 2D skin preview, check skin-preview
+
+class App extends React.Component{
+    static render(){
+        return(
+            <div>
+                <div id="skin-preview">
+                    <div id="skin-head">
+
+                    </div>
+                    <div id="skin-body">
+
+                    </div>
+                    <div id="skin-hands">
+
+                    </div>
+                    <div id="skin-legs">
+
+                    </div>
+                </div>
+                <div id="skin-elements">
+
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('content')
+);
+
