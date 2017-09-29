1267e02b3acd482d4e5d90794a1a9ffcc1193c51
Ikamari <SilverFire098@gmail.com>
2017-09-29 09:44:26 +0300

    Added skin body for preview

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,38 @@
 //TODO: UI, 2D skin preview, check skin-preview
 
 class App extends React.Component{
-    static render(){
+    render(){
         return(
             <div>
                 <div id="skin-preview">
                     <div id="skin-head">
 
                     </div>
-                    <div id="skin-body">
+                    <div id="skin-middle">
+                        <div id="skin-left-hand">
 
+                        </div>
+                        <div id="skin-body">
+
+                        </div>
+                        <div id="skin-right-hand">
+
+                        </div>
                     </div>
-                    <div id="skin-hands">
+                    <div id="skin-bottom">
+                        <div id="skin-left-leg">
 
-                    </div>
-                    <div id="skin-legs">
+                        </div>
+                        <div id="skin-right-leg">
 
+                        </div>
                     </div>
                 </div>
                 <div id="skin-elements">
 
                 </div>
             </div>
-        )
+        );
     }
 }
 
