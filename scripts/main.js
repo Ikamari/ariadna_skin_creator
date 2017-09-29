e12a1c2568a3c1f6d2f64386ed84adf10782aab0
Ikamari <SilverFire098@gmail.com>
2017-09-29 19:34:51 +0300

    Testing skin display

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,40 @@
 //TODO: UI, 2D skin preview, check skin-preview
 
 class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            skin_head: "head1",
+            skin_body: "body1",
+            skin_hand: "hand1",
+            skin_leg: "leg1"
+        }
+    };
+
     render(){
         return(
             <div>
                 <div id="skin-preview">
                     <div id="skin-head">
-
+                        <img href={"./img/" + this.state.skin_head + ".png"}/>
                     </div>
                     <div id="skin-middle">
                         <div id="skin-left-hand">
-
+                            <img href={"./img/" + this.state.skin_hand + ".png"}/>
                         </div>
                         <div id="skin-body">
-
+                            <img href={"./img/" + this.state.skin_body + ".png"}/>
                         </div>
                         <div id="skin-right-hand">
-
+                            <img href={"./img/" + this.state.skin_hand + ".png"}/>
                         </div>
                     </div>
                     <div id="skin-bottom">
-                        <div id="skin-left-leg">
-
+                        <div id="skin-left-leg" >
+                            <img href={"./img/" + this.state.skin_leg + ".png"}/>
                         </div>
                         <div id="skin-right-leg">
-
+                            <img href={"./img/" + this.state.skin_leg + ".png"}/>
                         </div>
                     </div>
                 </div>
